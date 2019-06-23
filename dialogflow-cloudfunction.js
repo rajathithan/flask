78f6b6e73cd643ebb52d68dc56f2080b5bef7792
rajathithan <rajathithan@gmail.com>
2019-06-23 09:00:44 -0500

    second commit

--- dialogflow-cloudfunction.js
+++ dialogflow-cloudfunction.js
@@ -1,40 +1,60 @@
 'use strict';
  
 var https = require ('https');
+
 const functions = require('firebase-functions');
+
 const DialogFlowApp = require('actions-on-google').DialogFlowApp;
 
-console.log('set me');
+console.log('set https, firebase-functions, Dialogflowapp');
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+
   console.log('Inside Main function');
+
   console.log('Request Headers: ' + JSON.stringify(request.headers));
+
   console.log('Request Body: ' + JSON.stringify(request.body));
   
   let action = request.body.queryResult.action;
+
   //const agent = new WebhookClient({ request, response });
+
   var chat = "This is a sample response";
+
   console.log(action);
+
   response.setHeader('Content-Type','application/json');
   
     if ((action!= 'input.getOrderNo') && (action!= 'input.getGiftcardBalance')){
-        console.log('Unknown input...exiting');
-        response.send(buildChatResponse("I'm sorry, I don't know this"));
+
+		console.log('Unknown input...exiting');
+		
+		response.send(buildChatResponse("I'm sorry, I don't know this, Can i transfer it to contact center"));
+		
         return;
     }
 
     const parameters = request.body.queryResult.parameters;
 
     if (action== 'input.getOrderNo'){
-        console.log('Successfully entered getOrderNo');
-        var orderNo = parameters.number;
-        getOrderNo (orderNo, response);    
+
+		console.log('Successfully entered getOrderNo');
+		
+		var orderNo = parameters.number;
+		
+		getOrderNo (orderNo, response); 
+		   
     }
 
     if (action== 'input.getGiftcardBalance'){
-        console.log('Successfully entered getGiftcardBalance');
-        var gcNo = parameters.number;
-        getGiftcardBalance (gcNo, response);
+
+		console.log('Successfully entered getGiftcardBalance');
+		
+		var gcNo = parameters.number;
+		
+		getGiftcardBalance (gcNo, response);
+		
     }
 
 });
@@ -51,21 +71,27 @@ function getOrderNo (orderNo, CloudFnResponse) {
 	console.log ('path string:' + pathString);
 
 	var request = https.get({
+
 		host: "rajathithanrajasekar-eval-prod.apigee.net",
+
 		path: pathString
+
 		}, function (response) {
+
 		var json = "";
+
 		response.on('data', function(chunk) {
+
 			console.log("Received JSON response: " + chunk);
+
 			json += chunk;
 
-			
 		});
 
 		response.on('end', function(){
+
 			var jsonData = JSON.parse(json);
-          
-		           
+          		           
 			var orderStatus = jsonData.status;
 
 			console.log ("The order status for order number -"+ orderNo +" is:" + orderStatus);
@@ -76,7 +102,7 @@ function getOrderNo (orderNo, CloudFnResponse) {
 
 		});
 
-});
+	});
 
 }
 
@@ -84,28 +110,33 @@ function getGiftcardBalance (gcNo, CloudFnResponse) {
 
 	console.log('In Function Get Giftcard Balance');
 
-	console.log("Giftcard No: " + gcNo);
-	
+	console.log("Giftcard No: " + gcNo);	
 	
 	var pathString = '/retailclient-giftcards-balance/' + gcNo;
 
 	console.log ('path string:' + pathString);
 
 	var request = https.get({
-		host: "rajathithanrajasekar-eval-prod.apigee.net",
-		path: pathString
-		}, function (response) {
-		var json = "";
-		response.on('data', function(chunk) {
-			console.log("Received JSON response: " + chunk);
-			json += chunk;
 
+		host: "rajathithanrajasekar-eval-prod.apigee.net",
+
+		path: pathString
+
+		}, function (response) {
+
+		var json = "";
+
+		response.on('data', function(chunk) {
+
+			console.log("Received JSON response: " + chunk);
+
+			json += chunk;
 			
 		});
 
 		response.on('end', function(){
-			var jsonData = JSON.parse(json);
-          
+
+			var jsonData = JSON.parse(json);          
 		           
 			var gcBalance = jsonData.amount;
 
@@ -117,10 +148,12 @@ function getGiftcardBalance (gcNo, CloudFnResponse) {
 
 		});
 
-});
+	});
 
 }
 
 function buildChatResponse(chat) {
+
 	return JSON.stringify({"fulfillmentText": chat});
+	
 }
